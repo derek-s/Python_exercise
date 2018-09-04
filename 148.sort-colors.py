15bfddc5cb7047b54e3435ef2013a96c0f5087af
Derek.S <derekseli@outlook.com>
2018-09-04 23:33:41 +0800

    颜色分类

---
+++ 148.sort-colors.py
@@ -0,0 +1,33 @@
+# #!/usr/bin/python
+# # -*- coding: utf-8 -*-
+# File:148.sort-colors.py
+# Project:Python_Exercise
+# File Created:2018-09-04 11:18:57
+# Author:Derek.S(derekseli@outlook.com)
+# -----
+# Last Modified:2018-09-04 11:18:57
+# -----
+
+# 给定一个包含红，白，蓝且长度为 n 的数组，将数组元素进行分类使相同颜色的元素相邻，并按照红、白、蓝的顺序进行排序。
+# 我们可以使用整数 0，1 和 2 分别代表红，白，蓝。
+# 不能使用代码库中的排序函数来解决这个问题。 
+# 排序需要在原数组中进行。
+
+# 样例 给你数组 [1, 0, 1, 2], 需要将该数组原地排序为 [0, 1, 1, 2]。
+
+class Solution:
+    """
+    @param nums: A list of integer which is 0, 1 or 2 
+    @return: nothing
+    """
+    def sortColors(self, nums):
+        # write your code here
+
+
+
+
+
+
+if __name__ == "__main__":
+    nums = [2,0,0,1,2,0,2]
+    Solution().sortColors(nums)
